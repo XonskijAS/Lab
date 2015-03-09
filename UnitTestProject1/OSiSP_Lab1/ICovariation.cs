96d08b91be0b21f8cbf3f9faca52b3c5323140c0
XonskijAS <fgqfgqfgq@mail.ru>
2015-03-09 21:22:54 +0300

    Lab

---
+++ UnitTestProject1/OSiSP_Lab1/ICovariation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSiSP_Lab1
+{
+    public interface ICovariation<out T>
+    {
+    }
+}
---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OSiSP_Lab1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IContravariation<ChildClass> obj = new Contravariation<BaseClass<string>>();
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            ICovariation<BaseClass<string>> obj = new Covariation<ChildClass>();
+        }
+
+        [TestMethod]
+        //[ExpectedException(typeof())]
+        public void TestMethod3()
+        {
+            IContravariation<ChildClass> obj = new Contravariation<BaseClass<string>>();
+            obj = (Contravariation<ChildClass>)obj;
+        }
+
+        [TestMethod]
+       // [ExpectedException(typeof())]
+        public void TestMethod4()
+        {
+            ICovariation<BaseClass<string>> obj = new Covariation<ChildClass>();
+            obj = (Covariation<ChildClass>)obj;
+        }
+    }
+}
