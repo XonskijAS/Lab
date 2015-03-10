b2bf7ac69d0e21415430bda1ebc181d0299dccd0
XonskijAS <fgqfgqfgq@mail.ru>
2015-03-10 13:43:34 +0300

    Xonskij_travels

---
+++ Xonskij_travels/Xonskij_travels/Clients.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xonskij_travels
+{
+    class Clients:Travels
+    {
+        private string Name;
+        private string Surname;
+        private string Patronymic;
+        private string Passport;
+
+    }
+}
---
+++ Xonskij_travels/Xonskij_travels/Route.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xonskij_travels
+{
+    class Route: Travels
+    {
+        private string DepartureDate;
+        private string DaysOfStay;
+
+    }
+}
